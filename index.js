d8441958be1c4e50191327b1ac8e71b294c76f97
smartcode54 <smartcode54@gmail.com>
2025-08-09 22:37:28 +0700

    add contact from submit

--- index.js
+++ index.js
@@ -160,3 +160,5 @@ document.getElementById('searchInput').addEventListener('keypress', function(eve
 const options = { timeZone: 'America/New_York', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
     const newYorkTime = new Date().toLocaleTimeString('en-US', options);
     console.log("Current time in New York:", newYorkTime);
+
+
